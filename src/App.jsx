87aa7afafecfa4ec2ea57a894c44da8583eb8cbd
RohithReddy-45 <samreddyrohith@gmail.com>
2024-08-28 17:20:01 +0530

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./components/About/About";
 import Projects from "./components/Projects/Projects";
 import Contact from "./components/Contact/Contact";
 import Footer from "./components/Footer";
+import { Analytics } from "@vercel/analytics/react";
 
 function App() {
 	return (
@@ -14,6 +15,7 @@ function App() {
 			<Projects />
 			<Contact />
 			<Footer />
+			<Analytics />
 		</div>
 	);
 }
