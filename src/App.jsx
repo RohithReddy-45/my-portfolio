85e7805384afaed440926ebdadde7abf8bc5f338
RohithReddy-45 <samreddyrohith@gmail.com>
2024-08-28 17:04:14 +0530

    feat: add Vercel analytics and refactor Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,21 @@
-import Navbar from './components/Navbar';
-import Home from './components/Home/Home';
-import About from './components/About/About';
-import Projects from './components/Projects/Projects';
-import Contact from './components/Contact/Contact';
-import Footer from './components/Footer';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home/Home";
+import About from "./components/About/About";
+import Projects from "./components/Projects/Projects";
+import Contact from "./components/Contact/Contact";
+import Footer from "./components/Footer";
 
 function App() {
-  return (
-    <div className="min-w-[320px] bg-white text-primary">
-      <Navbar />
-      <Home />
-      <About />
-      <Projects />
-      <Contact />
-      <Footer />
-    </div>
-  );
+	return (
+		<div className="min-w-[320px] bg-white text-primary">
+			<Navbar />
+			<Home />
+			<About />
+			<Projects />
+			<Contact />
+			<Footer />
+		</div>
+	);
 }
 
 export default App;
--- src/components/ui/NavLinks.jsx
+++ src/components/ui/NavLinks.jsx
@@ -1,21 +1,35 @@
-import Button from './Button';
-import NavList from './NavList';
-import { links } from '../../data/data';
+import Button from "./Button";
+import { links } from "../../data/data";
+import { Link } from "react-scroll";
+import { memo } from "react";
 
-function NavLinks({ className, onClick }) {
-  return (
-    <>
-      <ul className={className}>
-        {links.map((link) => (
-          <NavList key={link.id} listName={link.link} onClick={onClick} />
-        ))}
-      </ul>
+const NavLinks = memo(({ className, handleNav }) => {
+	return (
+		<>
+			<ul className={className}>
+				{links.map((link) => (
+					<li
+						key={link.id}
+						className="cursor-pointer rounded-lg px-3 py-2 text-lg font-medium transition-all duration-300 hover:bg-zinc-100 active:bg-zinc-200 md:text-base"
+					>
+						<Link
+							to={link.link}
+							href={`/${link.link}`}
+							smooth
+							duration={500}
+							onClick={handleNav}
+						>
+							{link.link}
+						</Link>
+					</li>
+				))}
+			</ul>
+			<Button type="primary" link="https://github.com/RohithReddy-45">
+				My github
+			</Button>
+		</>
+	);
+});
 
-      <Button type="primary" link="https://github.com/RohithReddy-45">
-        My github
-      </Button>
-    </>
-  );
-}
-
+NavLinks.displayName = "NavLinks";
 export default NavLinks;
